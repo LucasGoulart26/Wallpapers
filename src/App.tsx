b90c689685254892e00c76d4b6b04c6642945afc
LucasGoulart24 <Goulart7720@gmail.com>
2026-08-17 18:10:25 -0300

    wallpaper

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { FaSearch } from "react-icons/fa";
+
+function App() {
+  const[active, setActive] = useState("All");
+
+  const buttons = [
+    "All",
+    "Nature",
+    "Space",
+    "Minimalist",
+    "City"
+  ]
+  return(
+    <div className="w-full h-screen bg-zinc-800">
+      <div className="w-full h-20 flex items-center justify-between p-5 bg-zinc-900">
+        <h1 className="text-3xl text-transparent bg-clip-text bg-linear-to-r from-blue-500 to-purple-500">Wallpapers</h1>
+        <div className="w-96 h-11 flex items-center justify-center">
+          <input type="text" placeholder="Search wallpapers" className="w-full h-full outline-0 p-3 bg-zinc-700 border-2 border-zinc-600 rounded-bl-lg rounded-tl-lg text-white placeholder:text-white"/>
+          <button className="w-12 h-full cursor-pointer flex items-center justify-center bg-blue-500 active:bg-blue-600 transition-all duration-300 rounded-br-lg rounded-tr-lg">
+            <FaSearch className="text-2xl text-white"/>
+          </button>
+        </div>
+      </div>
+      <div className="w-full p-5 flex items-center justify-center flex-wrap gap-5">
+        {buttons.map((btn) => (
+          <button key={btn} onClick={() => setActive(btn)} className={`w-40 h-11 rounded-lg cursor-pointer transition-all duration-300 ${active === btn ? "bg-blue-500" : "bg-zinc-600 text-white"}`}>{btn}</button>
+        ))}
+      </div>
+      <div className="w-full p-5 flex items-center justify-center flex-col bg-zinc-800">
+        <div className="w-[90%] h-20 flex items-center justify-items-start p-3 border-l-4 border-l-blue-500 bg-zinc-700">
+          <h2 className="text-3xl text-white">Recent Wallpapers</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App
---
+++ src/data/wallpapers.ts
@@ -0,0 +1,6 @@
+export const wallpapers = [
+    {
+        wallpaper: "",
+        categoria: "Natureza"
+    }
+];
