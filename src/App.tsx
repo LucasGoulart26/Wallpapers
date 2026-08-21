c73e7b3f4015657b5b829f257612f13d1cc49ce7
LucasGoulart24 <Goulart7720@gmail.com>
2026-08-21 12:04:39 -0300

    wallpaper

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { FaSearch } from "react-icons/fa";
 import { wallpapers } from "./data/wallpapers";
+import { special } from "./data/special";
 
 function App() {
   const[active, setActive] = useState("All");
@@ -43,6 +44,26 @@ function App() {
             </div>
           ))}
         </div>
+      </div>
+      <div className="w-full h-screen p-5 flex items-center justify-center flex-col bg-zinc-800">
+        <div className="w-full h-20 flex items-center justify-items-start p-4 rounded-lg border-l-4 border-l-blue-500 bg-zinc-700">
+          <h1 className="text-3xl text-white">Special Wallpapers</h1>
+        </div>
+        <div className="w-full flex items-center justify-center gap-5 mt-5">
+          {special.map((specialWallpaper, index) => (
+            <div key={index} className="w-2xl rounded-2xl bg-zinc-900 overflow-hidden relative border-2 border-zinc-600">
+              <img src={specialWallpaper.imagem} alt="Imagem" className="w-full h-80"/>
+              <div className="flex items-center justify-center gap-2 p-4">
+                {specialWallpaper.tags.map((specialTag, indexTag) => (
+                  <button key={indexTag} className="w-36 p-1.5 rounded-lg bg-zinc-700 border-2 border-zinc-600 text-white cursor-pointer active:bg-zinc-600 transition-all duration-300">{specialTag}</button>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="w-full h-16 bg-zinc-900 flex items-center justify-center">
+        <h2 className="text-2xl text-transparent bg-clip-text bg-linear-to-l from-blue-500 to-purple-500">&copy;2026 - Copyright</h2>
       </div>
     </div>
   );
---
+++ src/data/special.ts
@@ -0,0 +1,12 @@
+import wallpaperSpecial from "../assets/wallpaperFireworks.jpg";
+
+export const special = [
+  {
+    imagem: wallpaperSpecial,
+    tags: ["4K - 3840x2160", "FHD - 1920x1080", "HD - 1280x720"],
+  },
+  {
+    imagem: wallpaperSpecial,
+    tags: ["4K - 3840x2160", "FHD - 1920x1080", "HD - 1280x720"],
+  },
+];
